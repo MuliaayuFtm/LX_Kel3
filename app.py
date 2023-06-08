c1ee4bd5392ce5581bd90cb021b03a7310278bf3
MuliaayuFtm <ayumulia39@gmail.com>
2023-06-09 05:50:24 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask,redirect,url_for,render_template,request
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+  
+   app.run('0.0.0.0', port=5000,debug=True)
