7fca4bc43c97943910989421d1590e5bcf8cf0fb
sihombing18 <126742581+sihombing18@users.noreply.github.com>
2023-06-20 09:48:52 +0700

    app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
-
-app = Flask(__name__, template_folder="templates")
 from pymongo import MongoClient
 
-client = MongoClient(
-    "mongodb+srv://test:sparta@cluster0.oblwrcu.mongodb.net/?retryWrites=true&w=majority"
-)
-db = client.testdb
+app = Flask(__name__)
 
+
+MONGODB_CONNECTION_STRING = 'mongodb://niksondinarfranselasihombing18:niksondinarfranselasihombing18@ac-zvfqymp-shard-00-00.tekjg64.mongodb.net:27017,ac-zvfqymp-shard-00-01.tekjg64.mongodb.net:27017,ac-zvfqymp-shard-00-02.tekjg64.mongodb.net:27017/?ssl=true&replicaSet=atlas-731qa5-shard-0&authSource=admin&retryWrites=true&w=majority'
+client = MongoClient(MONGODB_CONNECTION_STRING)
+db = client.finalproject
 
 @app.route("/")
 def home():
@@ -33,13 +32,12 @@ def login():
 
         if user:
             # login sukses
-            return render_template("destinasi.html")
+            return render_template("About.html")
         else:
             # login gagal
             return "Email atau password salah"
 
     return render_template("login.html")
-
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -66,14 +64,60 @@ def register():
     return render_template("register.html")
 
 
-@app.route("/destinasi")
-def destinasi():
-    return render_template("destinasi.html")
+@app.route("/halaman admin", methods=["GET", "POST"])
+def admin():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        # Proses otentikasi pengguna
+        user = users_collection.find_one({"email": email, "password": password})
+
+        if user:
+            # login sukses
+            return render_template("halaman admin.html")
+        else:
+            # login gagal
+            return "Email atau password salah"
+
+    return render_template("login admin.html")
+
+
+
+@app.route("/Destinasi")
+def Destinasi():
+    return render_template("Destinasi.html")
 
 
 @app.route("/reservasi")
 def reservasi():
     return render_template("reservasi.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/contact us")
+def contact():
+    return render_template("contact us.html")
+
+
+@app.route("/chek harga")
+def chek():
+    return render_template("chek harga.html")
+
+@app.route("/home")
+def main():
+    return render_template("home.html")
+
+@app.route("/pemesanan")
+def pemesanan():
+    return render_template("pemesanan.html")
+
+
+@app.route("/layout")
+def layout():
+    return render_template("layout.html")
 
 
 if __name__ == "__main__":
